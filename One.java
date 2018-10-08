31245b185deff2de6dc1c5090b5472917c7f0412
MD.ROKIBUL ISLAM <mdrokibulislam25@gmail.com>
2018-10-09 02:35:02 +0600

    Add files via upload

---
+++ One.java
@@ -0,0 +1,20 @@
+package first;
+
+import java.util.Scanner;
+
+public class One {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student ab=new Student();
+		ab.setId(5);
+		ab.setName("Rakib");
+		ab.setAge(23);
+		System.out.println("Name : "+ab.getName()+" Age: "+ab.getAge()+" Id: "+ab.getId());
+
+	}
+
+}
---
+++ method.java
@@ -0,0 +1,16 @@
+package first;
+
+public class method {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=add(5,6);
+		System.out.println(a);
+	}
+	public static int add(int a,int b){
+		return a+b;
+	}
+}
