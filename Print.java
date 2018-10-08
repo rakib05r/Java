c9f0d1537699ffb611984d830b2f96a273ef050e
MD.ROKIBUL ISLAM <mdrokibulislam25@gmail.com>
2018-10-09 02:26:54 +0600

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,13 @@
+package first;
+
+public class Print {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(2 << 4);
+	}
+
+}
