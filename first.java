5885ad59d8eb7730ccecf7374f6a8fa7214e2240
MD.ROKIBUL ISLAM <mdrokibulislam25@gmail.com>
2018-10-09 02:25:17 +0600

    Add files via upload

---
+++ first.java
@@ -0,0 +1,48 @@
+package ok;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Font;
+
+public class first extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					first frame = new first();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public first() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("FIRST");
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 14));
+		btnNewButton.setBounds(205, 85, 89, 65);
+		contentPane.add(btnNewButton);
+	}
+}
---
+++ name.java
@@ -0,0 +1,17 @@
+package ok;
+
+import java.util.Scanner;
+
+public class name {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println("MY NAME IS BLAH BLAH BLAH \n"+a);
+	}
+
+}
