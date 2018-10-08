53a8f4749ded72471a291618ab3bb18bb1304c8e
MD.ROKIBUL ISLAM <mdrokibulislam25@gmail.com>
2018-10-09 02:47:15 +0600

    Add files via upload

---
+++ FirstMethod.java
@@ -0,0 +1,17 @@
+
+public class FirstMethod {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a,b="Hello";
+		a=first("Rakib");
+		System.out.println(b+" "+a);
+	}
+	public static String first(String a){
+		return(a);
+	}
+
+}
---
+++ ReturnMethod.java
@@ -0,0 +1,17 @@
+
+public class ReturnMethod {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		a=sum(100,200);
+		System.out.println("The Sum Is "+a);
+	}
+	public static int sum(int a,int b){
+		return(a+b); 
+	}
+
+}
